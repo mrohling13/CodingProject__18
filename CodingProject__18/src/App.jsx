0bfd7ec6bd3b87f22907ac00ddae17d6e4f27757
mrohling13 <malachi.rohling@yahoo.com>
2025-04-06 14:58:05 -0400

    Task 2: Use the Component

--- CodingProject__18/src/App.jsx
+++ CodingProject__18/src/App.jsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import React from 'react'
+
+import Greeting from './components/Greeting'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -21,13 +25,11 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Greeting /> 
     </>
   )
 }
